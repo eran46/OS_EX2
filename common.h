92452604ca6efda7dc7be12d017220db69be6aad
eran46 <eran.hir@gmail.com>
2024-12-16 09:57:33 +0200

    update

--- common.h
+++ common.h
@@ -1,3 +1,4 @@
 #define CMD_ARGS_NUM 4
 #define MAX_LINE_LENGTH 1024
 #define  MAX_CMD_LENGTH 256
+
--- utils.c
+++ utils.c
@@ -1,1 +1,12 @@
-# utilities if needed
+
+int str_to_int(char* num_str) {
+	int num = 0;
+	int digit_factor = 1;
+	int len = strlen(num_str);
+	for(int i = len - 1; i >= 0; i--){
+		num += (num_str[i] - '0') * digit_factor;
+		digit_factor *= 10;
+	}
+	return num;
+}
+
