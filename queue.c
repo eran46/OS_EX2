7e155756f74aae0f6c4b20f82a54b0e79bae106c
Or <or666atias@gmail.com>
2024-12-23 09:01:45 +0200

    worker 2.0

--- queue.c
+++ queue.c
@@ -6,18 +6,18 @@
 #include <sys/time.h>
 
 typedef struct Node {
-    int task_id;
+    int task_id;         //distinguish it from other tasks- for tracking the lifecycle of a task
     char job_line[1024];
     struct Node* next;
 } Node;
 
-typedef struct {
+typedef struct {   
     Node* front;
     Node* rear;
     int count;
     pthread_mutex_t lock;
     pthread_cond_t cond_nonempty;
-} TaskQueue;
+} TaskQueue;    //create queue of threads
 
 void init_queue(TaskQueue* queue) {
     queue->front = queue->rear = NULL;
@@ -78,7 +78,7 @@ void destroy_queue(TaskQueue* queue) {
     pthread_cond_destroy(&queue->cond_nonempty);
 }
 
-long long elapsed_time_ms(struct timeval start_time) {
+long long elapsed_time_ms(struct timeval start_time) { //time the task in the queue
     struct timeval current_time;
     gettimeofday(&current_time, NULL);
     long long seconds = current_time.tv_sec - start_time.tv_sec;
@@ -92,50 +92,3 @@ typedef struct { //structure to pass multiple arguments to each worker thread
     struct timeval start_time;
 } ThreadArgs;
 
-void* worker_thread(void* arg) {
-    ThreadArgs* args = (ThreadArgs*)arg;
-    TaskQueue* queue = args->queue;
-    int thread_id = args->thread_id;
-    struct timeval start_time = args->start_time;
-    char filename[100];  // we need to check how much char we need
-    snprintf(filename, sizeof(filename), "thread%02d.txt", thread_id);
-    FILE* logfile = fopen(filename, "w");
-    if (logfile == NULL) {
-        perror("Failed to open log file");
-        pthread_exit(NULL);
-    }
-    while (1) {
-        Node* task_node = dequeue(queue);
-        long long time_ms_start = elapsed_time_ms(start_time);
-        fprintf(logfile, "TIME %lld: START job %s\n", time_ms_start, task_node->job_line);
-        fflush(logfile);
-
-        sleep(1);  // Simulate task processing time
-
-        long long time_ms_end = elapsed_time_ms(start_time);
-        fprintf(logfile, "TIME %lld: END job %s\n", time_ms_end, task_node->job_line);
-        fflush(logfile);
-
-        free(task_node);
-    }
-
-    fclose(logfile);
-    return NULL;
-}
-
-void start_worker_threads(TaskQueue* queue, int num_threads, struct timeval start_time) {
-    pthread_t threads[num_threads];
-    ThreadArgs thread_args[num_threads];
-
-    for (int i = 0; i < num_threads; i++) {
-        thread_args[i].queue = queue;
-        thread_args[i].thread_id = i + 1;
-        thread_args[i].start_time = start_time;
-        pthread_create(&threads[i], NULL, worker_thread, &thread_args[i]);
-    }
-
-    for (int i = 0; i < num_threads; i++) {
-        pthread_join(threads[i], NULL);
-    }
-}
-//111
